dd6a485a47ccf022fed18fe86282b132bdee05f6
Neha06 <nehajasmin06112002@gmail.com>
2026-09-17 22:14:16 +0530

    Center core team members

--- src/app/pages/Team.tsx
+++ src/app/pages/Team.tsx
@@ -148,7 +148,7 @@ export function Team() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
             {coreCommittee.map((member, index) => (
               <div
                 key={index}
